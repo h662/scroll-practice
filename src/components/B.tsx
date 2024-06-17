35a938bbda07bc9ad366cb9d7473852ee44eaf4e
h662 <h662hong@gmail.com>
2024-06-17 17:46:31 +0900

    intersection observer

--- src/components/B.tsx
+++ src/components/B.tsx
@@ -1,9 +1,23 @@
 import { Flex } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useObserve } from "../hooks";
+import { scroller } from "react-scroll";
 
 const B: FC = () => {
+  const { dom, isObserved } = useObserve();
+
+  useEffect(() => {
+    if (isObserved) {
+      scroller.scrollTo("B", {
+        duration: 800,
+        delay: 0,
+        smooth: "easeInOutQuart",
+      });
+    }
+  }, [isObserved]);
+
   return (
-    <Flex id="B" bgColor="blue.100" w="100%" h="100vh">
+    <Flex ref={dom} id="B" bgColor="blue.100" w="100%" h="100vh">
       B
     </Flex>
   );
--- src/components/C.tsx
+++ src/components/C.tsx
@@ -1,9 +1,23 @@
 import { Flex } from "@chakra-ui/react";
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useObserve } from "../hooks";
+import { scroller } from "react-scroll";
 
 const C: FC = () => {
+  const { dom, isObserved } = useObserve();
+
+  useEffect(() => {
+    if (isObserved) {
+      scroller.scrollTo("C", {
+        duration: 800,
+        delay: 0,
+        smooth: "easeInOutQuart",
+      });
+    }
+  }, [isObserved]);
+
   return (
-    <Flex id="C" bgColor="green.100" w="100%" h="100vh">
+    <Flex ref={dom} id="C" bgColor="green.100" w="100%" h="100vh">
       C
     </Flex>
   );
